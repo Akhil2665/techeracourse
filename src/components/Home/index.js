4e839573955aaa5e8c98db9075e7f4e336ce245b
Akhil2665 <akhileee.16@gmail.com>
2025-03-07 17:17:34 +0000

    version 1.0.1

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -55,21 +55,23 @@ class Home extends Component {
   )
 
   renderFailureView = () => (
-    <div className="course-details-failure-view-container">
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/tech-era/failure-img.png"
-        alt="failure view"
-        className="failure-view-image"
-      />
-      <h1 className="failure-view-heading">Oops! Something Went Wrong</h1>
-      <p className="failure-view-description">
-        We cannot seem to find the page you are looking for
-      </p>
+    <>
+      <div className="course-details-failure-view-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/tech-era/failure-img.png"
+          alt="failure view"
+          className="failure-view-image"
+        />
+        <h1 className="failure-view-heading">Oops! Something Went Wrong</h1>
+        <p className="failure-view-description">
+          We cannot seem to find the page you are looking for
+        </p>
 
-      <button type="button" className="retry-button" onClick={this.getData}>
-        Retry
-      </button>
-    </div>
+        <button type="button" className="retry-button" onClick={this.getData}>
+          Retry
+        </button>
+      </div>
+    </>
   )
 
   renderSuccessView = () => {
